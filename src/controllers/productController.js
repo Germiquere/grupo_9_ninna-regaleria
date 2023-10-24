edcd6db019271d5aa6b18088bb2ada9ddd158b95
unknown <leandrojosezapata@gmail.com>
2023-10-24 19:44:15 -0300

    agrego products-wines.json

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,25 @@
 const controller = {
-    cart(req, res) {
+    cart: (req, res) => {
         res.render('./products/productCart')
     },
-    detail(req, res) {
-        res.render('./products/productDetail')
+    detail: (req, res) => {
+        res.render('./products/productDetail');
+    },
+
+    /* PRODUCT CREATE */
+    create: (req, res) => {
+        res.render('./products/product-create-form');
+    },
+    store: (req, res) => {
+        res.render('./index');
+    },
+    edit: (req, res) => {
+        res.render('./products/product-edit-form');
+    },
+    update: (req, res) => {
+        res.render('./index');
     }
 };
+
 
 module.exports = controller;
