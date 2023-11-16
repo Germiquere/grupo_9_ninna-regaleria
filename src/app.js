5958942d1ad46737a6e5911ddfffa9930f08666b
AugustoMarino <augustomarino07@hotmail.com>
2023-11-16 19:50:47 -0300

    Implementando session

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
 const express = require('express');
-const path = require('path');
+const session = require('express-session');
 const methodOverride =  require('method-override');
-const mainLogin = require('./routes/users');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
 const main = require('./routes/main');
+const mainLogin = require('./routes/users');
 const mainProduct = require('./routes/product');
 
 const app = express();
 
-const port = 3000;
+const  userLogedMiddleware = require('./middlewares/userLogedMiddleware');
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(methodOverride('_method'));
+app.use(session({ secret: 'SECRET' }))
+app.use(cookieParser())
+app.use(userLogedMiddleware);
 
 app.use('/', main);
 app.use('/', mainLogin);
 app.use('/', mainProduct);
 
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -4,14 +4,54 @@ const bcrypt = require('bcryptjs');
 
 const {validationResult} = require('express-validator');
 
-const pathFileUsers = path.join(__dirname, '../data/users.json');
-const getUser = function () {
-    return JSON.parse(fs.readFileSync(pathFileUsers, 'utf-8'));
+const usersDataFilePath = path.join(__dirname, '../data/users.json');
+function getUsers() {
+    return JSON.parse(fs.readFileSync(usersDataFilePath, 'utf-8'));
 }
 
 const controller = {
     login(req, res) {
+        if (req.session.user) {
+            return res.redirect('/profile')
+        }
         res.render('./users/login')
+    },
+
+    loginIn(req, res) {
+        const users = getUsers();
+        const user = users.find((element) => element.email === req.body.email);
+        const errors = {
+            unauthorized: {
+                msg: 'Usuario y/o contraseña incorrecto'
+            }
+        };
+        if (!user) {
+            return res.render('./users/login', { errors })
+        }
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.render('./users/login', { errors });
+        }
+        req.session.user = {
+            timestamp: Date.now(),
+            id: user.id,
+            name: user.fullname,
+            email: user.email,
+            age: user.age,
+            dni: user.dni
+        };
+        res.cookie('username', req.body.email)
+        return res.redirect('/profile')
+    },
+
+    profile(req, res) {
+        const { user } = req.session
+        res.render('./users/profile', { user })
+    },
+
+    logout(req, res) {
+        delete req.session.user;
+        res.clearCookie('username');
+        return res.redirect('/');
     },
     register(req, res) {
         res.render('./users/register')
@@ -24,7 +64,7 @@ const controller = {
                 oldData: req.body
             });
         }
-        const users = getUser();
+        const users = getUsers();
         const newUser = {
             id: [users.length]>0 ? users[users.length - 1].id + 1 : 1,
             ...req.body,
@@ -33,7 +73,7 @@ const controller = {
             category: "Usuario"
         };
         users.push(newUser);
-        fs.writeFileSync(pathFileUsers, JSON.stringify(users, null, 4));
+        fs.writeFileSync(usersDataFilePath, JSON.stringify(users, null, 4));
         return res.redirect('/');
     }
 };
